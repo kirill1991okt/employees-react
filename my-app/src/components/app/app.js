07d7da02617742ff7aa4591c67662823ac4a26c9
Kirill <kirill1991okt@gmail.com>
2022-10-26 13:50:31 +0300

    feat: add components employees-add-form, employees-list-item, employees-list

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,6 +1,9 @@
 import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
   return (
@@ -8,7 +11,10 @@ function App() {
       <AppInfo />
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
       </div>
+      <EmployeesList />
+      <EmployeesAddForm />
     </div>
   );
 }
